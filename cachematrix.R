4461614fb9c6ad2b3aadb4714ae37a3234b572c7
Winthrop7 <fritz@newtonstreet.org>
2014-11-21 12:52:00 -0800

    The makeCacheMatrix function
    Uncommented test

--- cachematrix.R
+++ cachematrix.R
@@ -4,9 +4,22 @@
 ## Write a short comment describing this function
 ## trial comment
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
+
+
 
 
 ## Write a short comment describing this function
