5ca9a8313cb796b9dd7e944ceb556ac244265ab5
Winthrop7 <fritz@newtonstreet.org>
2014-11-20 16:57:05 -0800

    A woking version of cacheSolve
    1) Does the value of the inverse exist in the "outside" environment? If
    so, get the value and return it.
    2) No inverse value? Then use solve(), cache it's value, and return it
    to the user.

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
+## trial comment
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -11,5 +12,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  inv <- x$getinv()
+  
+  # If the matrix inverse has been calculated, return its cached value
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
 }
+# Calculate, using solve(), the matrix inverse, if the inverse has not yet been calculated.
+data <- x$get()
+inv <- solve(data, ...)
+
+# Now cache the newly created inverse
+x$setinv(inv)
+
+# Now return the newly cached value
+inv
+}
+
